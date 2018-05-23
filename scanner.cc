759afe32ed667e65f892456b28435d7d8c1fbee2
MattAshman <matthew.ashman97@gmail.com>
2018-05-23 16:29:49 +0100

    Add scanner.cc

---
+++ scanner.cc
@@ -0,0 +1,120 @@
+#include "scanner.h"
+#include "names.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+scanner::scanner(names* names_mod, const char* defname) 
+{
+	
+	nmz = names_mod;	
+	eofile = false;
+	
+	clkname = nmz->lookup("CLK");
+	swtchname = nmz->lookup("SWITCH");
+	andname = nmz->lookup("AND");
+    nandname = nmz->lookup("NAND");
+    orname = nmz->lookup("OR");
+    norname = nmz->lookup("NOR");
+    dtypename = nmz->lookup("DTYPE");
+    xorname = nmz->lookup("XOR");
+    connectname = nmz->lookup("CONNECT");
+    monitorname = nmz->lookup("MONITOR");
+    
+	inf.open(defname);
+	 
+	if (!inf) {
+		cout << "Error: cannot open file " << defname << " for reading" << endl;
+		exit(1);
+	}
+    getch();
+}
+
+void scanner::getch()
+{
+	eofile = (!inf.get(curch));
+}
+
+void scanner::skipspaces()
+{
+	while (isspace(curch)) {
+		getch();
+	}
+}
+
+void scanner::getname(name& id)
+{
+	namestring str = ""; // to store the namestring
+	
+	while (!eofile) {
+		if (isdigit(curch) || isalpha(curch)) {
+			str += curch;
+			eofile = (!inf.get(curch)); 
+		}
+		else {
+			id = nmz->lookup(str);
+			return;
+		}
+	}
+	id = nmz->lookup(str);
+	return;
+}
+
+void scanner::getnumber(int& num) 
+{
+	num = curch - '0';
+	eofile = !inf.get(curch); // get next character
+	if(eofile) return;
+	while (isdigit(curch)) 
+	{
+		num = num*10 + curch -'0';
+		eofile = !inf.get(curch);
+		if(eofile) return;
+	}
+}
+
+void scanner::getsymbol(symbol& s, name& id, int& num)
+{
+	skipspaces();
+	if (eofile) {
+		s = eofsym;
+	}
+	else { 
+		if (isdigit(curch)) { // number
+			s=numsym;
+			getnumber(num); 
+		}
+		else {
+			if (isalpha(curch)) { // name
+				getname(id);
+				if (id == clkname || id == swtchname){
+					s=gensym;
+				} else{
+					if (id == andname || id == nandname || id == orname || id == norname || id == dtypename || id == xorname){
+						s = devsym;
+					} else {
+						if (id == connectname) {
+							s=consym; 
+						} else {
+							if (id == monitorname) {
+								s=monsym; 
+							} else {
+								s=namesym; // not a keyword
+							}
+						}
+					}
+				} 
+			}else {
+				switch(curch) {
+					case ';': s=semicol; break;
+					case '.': s=dotsym; break;
+					default: s=badsym; break;
+				}
+				getch();
+			}
+		}
+	}
+}
---
+++ scanner.h
@@ -0,0 +1,51 @@
+#ifndef scanner_h
+#define scanner_h
+
+#include "names.h"
+#include "scanner.h"
+#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+class scanner {
+	
+	char curch; // current character
+	bool eofile; // true when end of file has been reached
+	ifstream inf; // input file to be read from
+	names* nmz;
+	
+	name clkname;
+	name swtchname;
+	name andname;
+    name nandname;
+    name orname;
+    name norname;
+    name dtypename;
+    name xorname;
+    name connectname;
+    name monitorname;
+	
+public:
+
+	typedef enum {gensym, devsym, consym, monsym, 
+		namesym, numsym, eofsym, semicol, dotsym, badsym} symbol;
+		
+	// this is the constructor of the scanner class
+	scanner(names* names_mod,     // pointer to names class, so scanner can use it
+        const char* defname); // name of file from which to read the definitions
+	
+	~scanner(); // destructor of the class, closes input file
+	
+	void getch(); // set character to next character
+	void skipspaces(); // set character to next non-space character
+	void getname(name& id); // scans the string, adds to names_mod if not already there and returns name id
+	void getnumber(int& num); // scans the number in the file and returns it's value
+	void getsymbol(symbol& s, // the sort of symbol read from the file
+				   name&  id, // return the name id here if it is a name
+				   int& num); // return the value here if it is a number
+				   
+};
+
+#endif /* scanner_h */
