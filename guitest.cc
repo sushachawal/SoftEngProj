a3fd2a18437ecd643eb7aa6e93a933d1cf52142e
Vlad Tomescu <vit21@cam.ac.uk>
2018-05-29 10:25:43 +0100

    Test devices class etc.

--- guitest.cc
+++ guitest.cc
@@ -12,7 +12,14 @@ bool MyApp::OnInit()
   netz = new network(nmz);
   dmz = new devices(nmz, netz);
   mmz = new monitor(nmz, netz);
-
+  
+  bool ok = true;
+  dmz->makedevice(aswitch, 1, 1, ok);
+  dmz->makedevice(aswitch, 2, 1, ok);
+  devicekind k = dmz->devkind(2);
+  dmz->writedevice(k);
+  
+  
   // glutInit cannot cope with Unicode command line arguments, so we pass
   // it some fake ASCII ones instead
   char **tmp1; int tmp2 = 0; glutInit(&tmp2, tmp1);
@@ -20,4 +27,5 @@ bool MyApp::OnInit()
   MyFrame *frame = new MyFrame(NULL, "Logic simulator", wxDefaultPosition,  wxSize(800, 600), nmz, dmz, mmz);
   frame->Show(true);
   return(true); // enter the GUI event loop
+  
 }
